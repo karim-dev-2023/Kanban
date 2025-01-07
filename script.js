1c73c926b3c28985b7b371a5c96a43fb6b253b49
Karim <karim.ayoub.job@gmail.com>
2025-01-07 16:16:08 +0100

    Fonctionalites Filtrer

--- script.js
+++ script.js
@@ -3,20 +3,44 @@ console.log("Kanban JS loaded...");
 // Exemple éventuel de structure
 window.addEventListener("DOMContentLoaded", () => {
   // Ici, on récupère les éléments du DOM
-  const addCardBtn = document.getElementById('addCardBtn');
-  const searchInput = document.getElementById('searchInput');
-  const sortByPriorityBtn = document.getElementById('sortByPriorityBtn');
+  const addCardBtn = document.getElementById("addCardBtn");
+  const searchInput = document.getElementById("searchInput");
+  const sortByPriorityBtn = document.getElementById("sortByPriorityBtn");
 
   // Éventuellement, on écoute les événements
-  addCardBtn.addEventListener('click', () => {
+  addCardBtn.addEventListener("click", () => {
     // ...
   });
 
-  searchInput.addEventListener('input', () => {
-    // ...
+  searchInput.addEventListener("input", () => {
+    const filter = searchInput.value.toLowerCase();
+    var rows = document.querySelectorAll(".column");
+
+    rows.forEach(function (row) {
+      var heading = row.querySelector("h3");
+      var content = row.querySelector("p");
+      if (heading) {
+        var text = heading.textContent.toLowerCase();
+        if (text.includes(filter)) {
+          row.style.display = ""; 
+        } else {
+          row.style.display = "none";
+        }
+      }
+      
+      if (content) {
+        var text = content.textContent.toLowerCase();
+        if (text.includes(filter)) {
+          row.style.display = ""; 
+        } else {
+          row.style.display = "none";
+        }
+      }
+
+    });
   });
 
-  sortByPriorityBtn.addEventListener('click', () => {
+  sortByPriorityBtn.addEventListener("click", () => {
     // ...
   });
 });
